05db11ebb3e05de4be7f5b677544e636c29ab66c
aguzel <ahmethamdiguzel@gmail.com>
2023-01-24 02:19:44 +0000

    unnecessary .py deleted

--- main.py
+++ main.py
@@ -34,11 +34,6 @@ def main():
                         help='Learning rate used for the optimization. Default is {}'.format(learning_rate)
                        )
     parser.add_argument(
-                        '--kerneltype',
-                        type=str,
-                        help='Kernel type (e.g., RGB or LMS). Default is {}'.format(kernel_type)
-                       )
-    parser.add_argument(
                         '--filename',
                         type=argparse.FileType('r'),
                         help='Image filename. Default is {}'.format(filename)
@@ -60,15 +55,9 @@ def main():
                        )
 
     parser.add_argument(
-                        '--prescription',
-                        type=str,
-                        help='The type of refractive error. Default is {}'.format(prescription)
-                       )
-
-    parser.add_argument(
                         '--diopter',
                         type=float,
-                        help='Unit used to measure the correction. Default is {}'.format(diopter)
+                        help='Unit used to measure the correction [- for myopia, + for hyperopia]. Default is {}'.format(diopter)
                        )
     
     args = parser.parse_args()
@@ -76,8 +65,6 @@ def main():
         no_of_iterations = args.iterations
     if not isinstance(args.filename, type(None)):
         filename = str(args.filename.name)
-    if not isinstance(args.kerneltype, type(None)):
-        kernel_type = str(args.kerneltype)
     if not isinstance(args.device, type(None)):
         device_name = str(args.device)
     if not isinstance(args.directory, type(None)):
@@ -86,16 +73,17 @@ def main():
         backlight = args.backlight
     if not isinstance(args.learningrate, type(None)):
         learning_rate = args.learningrate
+    if not isinstance(args.diopter, type(None)):
+        diopter = args.diopter
     device = torch.device(device_name)
     print('Filename: {}'.format(filename))
     print('Iterations: {}'.format(no_of_iterations))
-    print('Kernel type: {}'.format(kernel_type))
     print('Learning rate: {}'.format(learning_rate))
     print('Device: {}'.format(device))
     print('Directory: {}'.format(output_directory))
     print('Backlight primary: {}'.format(backlight))
     print('prescription: {}'.format(prescription))
-    print('dipter: {}'.format(diopter))
+    print('diopter: {}'.format(diopter))
     process(
             no_of_iterations=no_of_iterations,
             filename=filename,
@@ -103,7 +91,8 @@ def main():
             backlight=backlight,
             learning_rate=learning_rate,
             kernel_type=kernel_type,
-            device=device
+            device=device,
+            diopter = diopter
            )
 
 
@@ -113,6 +102,7 @@ def process(
             directory,
             backlight,
             device,
+            diopter,
             learning_rate=0.01,
             kernel_fn='./output/kernel_lms.pt',
             kernel_type='LMS',
@@ -126,45 +116,33 @@ def process(
                 0.0,     # Vertical Trefoil
                 0.0,     # Vertical Coma
                 0.0      # Horizontal Coma
-            ],
-            spherical_power = -3.5,    # negative diopters => myopia, positive diopters => hyperopia
-            cylindrical_power=0,       # negative diopters => myopic astigmatism, positive diopters => hyperopic astigmatism
-            axis_angle=0,              # 0 to 180 degrees, refers to where on the cornea the astigmatism is located
+            ],             
+            cylindrical_power=0,       
+            axis_angle=0,           
             eye_pupil_diameter=6,
-            psf_size_degrees=0.45   # upper bound of 2.285
+            psf_size_degrees=0.45  
            ):
-    from color_space.display import sample_under_display
-    # loss_color = sample_under_display(device=device)
+    
+    spherical_power = diopter 
     target_image = odak.learn.tools.load_image(filename)[:,:,0:3] / 255.
-    if kernel_type == 'LMS':
-        if os.path.exists(kernel_fn):
-            kernel = torch.load(kernel_fn)
-        else:
-            kernel = get_display_kernel_4D(
-                                            target_image,
-                                            kernel_type,
-                                            zernike_coefficients=zernike_coefficients,
-                                            spherical_power=spherical_power,
-                                            cylindrical_power=cylindrical_power,
-                                            axis_angle=axis_angle,
-                                            eye_pupil_diameter=eye_pupil_diameter,
-                                            psf_size_degrees=psf_size_degrees,
-                                            backlight=backlight
-                                          )
-            odak.tools.check_directory('./output')
-            torch.save(kernel, kernel_fn)
-    elif kernel_type == 'RGB' or kernel_type == 'camera':
-        kernel = create_optical_aberration_kernel(target_image,
-                                                  zernike_coefficients,
-                                                  spherical_power=spherical_power,
-                                                  cylindrical_power=cylindrical_power,
-                                                  axis_angle=axis_angle,
-                                                  eye_pupil_diameter=eye_pupil_diameter,
-                                                  psf_size_degrees=psf_size_degrees,
-                                                  wavelengths=[620, 555, 470])
+    if os.path.exists(kernel_fn):
+        kernel = torch.load(kernel_fn)
+    else:
+        kernel = get_display_kernel_4D(
+                                        target_image,
+                                        kernel_type,
+                                        zernike_coefficients=zernike_coefficients,
+                                        spherical_power=spherical_power,
+                                        cylindrical_power=cylindrical_power,
+                                        axis_angle=axis_angle,
+                                        eye_pupil_diameter=eye_pupil_diameter,
+                                        psf_size_degrees=psf_size_degrees,
+                                        backlight=backlight
+                                        )
+        odak.tools.check_directory('./output')
+        torch.save(kernel, kernel_fn)
     optimizer = learned_prescription_optimizer(
                                                target_image,
-                                            #    loss_function=loss_color,
                                                loss_function=None,
                                                no_of_iterations=no_of_iterations,
                                                learning_rate=learning_rate,
